111856b51638119f91fe559e40a5012d18e8edf0
Damian Z <damian9203@gmail.com>
2021-12-20 14:43:12 +0100

    Add project files.

---
+++ L2_8_Zad_8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace L2_8_Zad_8
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program odwróci wprowadzony przez użytkownika ciąg znaków.");
+
+            Console.Write("Podaj łańcuch znaków: "); 
+            string userInput = Console.ReadLine();
+
+            int counterOne = userInput.Length - 1;
+            int counterTwo = 0;
+
+            while (counterOne >= 0)
+            {
+                char charOfTheStringBackward = userInput[counterOne];
+                char charOfTheStringForward = userInput[counterTwo];
+
+                counterOne--;
+                counterTwo++;
+
+                if (char.IsUpper(charOfTheStringForward))
+                {
+                    Console.Write(char.ToUpper(charOfTheStringBackward));
+                }
+                else
+                {
+                    Console.Write(char.ToLower(charOfTheStringBackward));
+                }
+            }
+        }
+    }
+}
